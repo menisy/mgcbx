aefd598eb9907708ae1941ed45e1d060cbc40436
menisy <elmenisy@gmail.com>
2018-05-10 14:15:40 +0200

    motion test

--- src/index.js
+++ src/index.js
@@ -1,8 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React, { Component, PropTypes } from 'react'
+import { render } from 'react-dom'
+import ReactAccelerometer from 'react-accelerometer'
+import { Motion, spring } from 'react-motion'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+/* Combining React-Accelerometer with React-Motion */
+const ReactAccelerometerMotion = ({ children }) => (
+  <ReactAccelerometer>
+    {({ x, y }) => (
+      <Motion style={{ x: spring(x), y: spring(y) }}>
+        {pos => children(pos)}
+      </Motion>
+    )}
+  </ReactAccelerometer>
+)
+
+const AwesomeComponent = () => (
+  <ReactAccelerometerMotion>
+    {({ x, y }) => (
+      <img
+        src='https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png'
+        style={{ transform: `translate3d(${x}px, ${y}px, 0)` }}
+      />
+    )}
+  </ReactAccelerometerMotion>
+)
+
+render(<AwesomeComponent />, document.querySelector('#root'))
