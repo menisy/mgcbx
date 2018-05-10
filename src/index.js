b11aded56122487837e0214b1c56421627666f8f
menisy <elmenisy@gmail.com>
2018-05-10 14:45:37 +0200

    Trial 1

--- src/index.js
+++ src/index.js
@@ -1,28 +1,36 @@
 import React, { Component, PropTypes } from 'react'
 import { render } from 'react-dom'
-import ReactAccelerometer from 'react-accelerometer-field'
+import ReactAccelerometer from 'react-accelerometer'
 import { Motion, spring } from 'react-motion'
+
+
 
 /* Combining React-Accelerometer with React-Motion */
 const ReactAccelerometerMotion = ({ children }) => (
+
   <ReactAccelerometer>
-    {({ x, y }) => (
-      <Motion style={{ x: spring(x), y: spring(y) }}>
-        {pos => children(pos)}
-      </Motion>
-    )}
-  </ReactAccelerometer>
+          {(position) => {
+            if (!position) {
+              return children({ x: 0, y: 0 })
+            }
+            return (
+              <Motion style={{ x: spring(position.x), y: spring(position.y) }}>
+                {pos => children(pos)}
+              </Motion>
+            )
+          }}
+        </ReactAccelerometer>
 )
 
 const AwesomeComponent = () => (
   <ReactAccelerometerMotion>
     {({ x, y }) => (
       <img
-        src='https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png'
+        src='https://www.google.com.eg/images/branding/googlelogo/2x/googlelogo_color_120x44dp.png'
         style={{ transform: `translate3d(${x}px, ${y}px, 0)` }}
       />
     )}
   </ReactAccelerometerMotion>
 )
 
-render(<AwesomeComponent />, document.querySelector('#root'))
+render(<AwesomeComponent />, document.querySelector('#app'))
